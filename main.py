c38cd9227eacc5d8ec228b2e5df2f72fb525a434
Z1RIX <alexfc13558@gmail.com>
2025-12-17 05:38:03 +0300

    1.0

--- main.py
+++ main.py
@@ -8,20 +8,46 @@ while True:
           "3. Удалить задачу",
           "4. Выход",sep="\n")
     choiceTask = input("Выберите действие: ")
+
+    # Добавление задач
     if choiceTask == "1":
         choice = input("Введите задачу: ")
-        print(f"Вы уверены что хотитет сохранить задачу '{choice}'?")
+        print(f"Вы уверены что хотите сохранить задачу '{choice}'?")
         print("1. ДА/2. НЕТ")
         sub = input()
         if sub == "1" or sub == "ДА":
             tasks.append(choice)
-            print(f"Задача {choice} успешно сохранена")
+            print(f"Задача {choice} успешно сохранена✅")
+
+
+    # показ задач
     elif choiceTask == "2":
+        if len(tasks) == 0:
+            print("Вы еще не добавили ни одной задачи!")
+        else:
+            print("------СПИСОК ЗАДАЧ------")
+            print()
+            for i in range(len(tasks)):
+                print(f"{i+1}. {tasks[i]}")
+        print()
+        print()
+
+    # Удаление задач
+    elif choiceTask == "3":
         for i in range(len(tasks)):
             print(f"{i+1}. {tasks[i]}")
-    elif choiceTask == "3":
-        sub3 = input("Для удаления ведите номер под которым находится задача: ")
+        print()
+        sub3 = int(input("Введите номер задачи для удаления: ")) - 1
+        print()
+        if 0 <= sub3 < len(tasks):
+            removed = tasks.pop(sub3)
+            print(f"Задача '{removed}' успешно удалена! ✅")
+        else:
+            print("Задачи под таким номером не существует❌")
 
+
+    elif choiceTask == "4":
+        break
 
 
 
